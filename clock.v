5ae2fb5e39ce172c5898e135d6c21f8619ec0ef0
0xDEADC0DEx <48600374+0xDEADC0DEx@users.noreply.github.com>
2021-06-22 18:31:07 +0200

    Added experimental pulse module

--- clock.v
+++ clock.v
@@ -9,6 +9,34 @@ end
 assign clk_out = r_count[ DIV - 1 ];
 endmodule
 
+    // generates one pulse if on_in is set to high
+    module pulse#( parameter LENGTH = 8 ) ( input clk_in, input on_in, output reg pulse_out );
+
+reg init = 1'b0;
+reg [ LENGTH - 1 : 0 ] r_counter;
+
+always@( posedge clk_in ) begin
+    if ( on_in && !init )
+    begin
+        pulse_out <= 1;
+        r_counter <= 2 ^ ^ LENGTH;
+        init = 1'b1;
+    end
+
+    if ( r_counter != 0 )
+    begin
+        r_counter = r_counter - 1;
+    end
+
+    if ( r_counter == 0 )
+    begin
+        pulse_out <= 0;
+        init = 1'b0;
+    end
+end
+endmodule
+
+
     // converts parallel data into serial data
     module piso#( parameter SIZE = 8 ) ( input [ SIZE - 1: 0 ] data_in, input clk_in, output reg r_out );
 
@@ -16,7 +44,7 @@ reg [ SIZE - 1: 0 ] r_count;
 reg [ SIZE - 1: 0 ] r_buff;
 
 always@( posedge clk_in ) begin
-    if ( r_count >= SIZE )
+    if ( r_count == SIZE )
     begin
         r_count <= 0;
     end
@@ -25,6 +53,7 @@ always@( posedge clk_in ) begin
     r_count <= r_count + 1;
 end
 endmodule
+
 
     // convert serial into parallel data
     module sipo#( parameter SIZE = 8 ) ( input data_in, input clk_in, output reg [ SIZE - 1: 0 ] r_out );
--- stepper.v
+++ stepper.v
@@ -18,7 +18,12 @@ reg [ 39: 0 ] data_outgoing;
 wire [ 39: 0 ] data_ingoing;
 reg send_enable;
 
-spi#( .SIZE( 40 ), .CLK_DIV( 3 ) ) spi_1 ( .data_in( data_outgoing ), .clk_in( clk_in ), .serial_in( serial_in ), .send_enable_in( 1'b0 ), .data_out( data_ingoing ), .clk_out( clk_out ), .serial_out( serial_out ) );
+spi#( .SIZE( 40 ), .CLK_DIV( 3 ) ) spi_1 ( .data_in( data_outgoing ), .clk_in( clk_25mhz ), .serial_in( gn[ 0 ] ), .send_enable_in( 1'b0 ), .data_out( data_ingoing ), .clk_out( gn[ 2 ] ), .serial_out( gn[ 3 ] ) );
+
+initial
+begin
+
+end
 
 
 endmodule
