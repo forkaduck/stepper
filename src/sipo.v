6fa4bafe1d89898cee18fdd1a9c7212041e8950e
0xDEADC0DEx <48600374+0xDEADC0DEx@users.noreply.github.com>
2021-07-04 18:24:49 +0200

    Added initial value for r_out in sipo

--- src/sipo.v
+++ src/sipo.v
@@ -4,6 +4,9 @@ module sipo#( parameter SIZE = 8 ) (
            input clk_in,
            output reg [ SIZE - 1: 0 ] r_out );
 
+initial
+    r_out = 'b0;
+
 always@( posedge clk_in ) begin
     r_out <= { data_in, r_out[ SIZE - 2: 0 ] };
 end
