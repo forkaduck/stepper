21fd7420787dc2258ea2ed83072c310932949dd8
0xDEADC0DEx <48600374+0xDEADC0DEx@users.noreply.github.com>
2021-07-08 19:44:26 +0200

    Added line number to assert macro

--- tests/macros.v
+++ tests/macros.v
@@ -1,5 +1,5 @@
 `define ASSERT(signal, value) \
         if (signal !== value) begin \
-            $display("%0t:\tASSERTION FAILED in %m | signal != value | 0x%x != 0x%x", $time, signal, value); \
+            $display("%0t:\tASSERTION FAILED in %m at line %0d | signal != value | 0x%x != 0x%x", $time, `__LINE__, signal, value); \
             $finish; \
         end
