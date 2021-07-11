c8689f78164a939de1aed68eea090a93bfa1cdc8
0xDEADC0DEx <48600374+0xDEADC0DEx@users.noreply.github.com>
2021-07-11 17:26:27 +0200

    Fixed missing sim timescale and output functions

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,49 +1,74 @@
-#!/bin/python3
+#!/sbin/python
 
 import os
 import re
 import subprocess
 
-cwd = os.path.dirname(os.path.abspath(__file__))
-src_dir = cwd + "/../src/"
-output_dir = cwd + "/sim_output/"
+src_dir = "../src/"
+output_dir = "sim_output/"
 
 
 def delold():
     print("[*] Deleting old simulation files")
 
-    for i in os.listdir(cwd):
+    for i in os.listdir(output_dir):
         vvp = re.search("\.vvp$", i)
         vcd = re.search("\.vcd$", i)
 
         if vvp is not None or vcd is not None:
-            os.remove(cwd + "/" + i)
+            os.remove(output_dir + i)
 
 
-# /sbin/iverilog -o sim_build/sim.vvp -D COCOTB_SIM=1 -s piso -f sim_build/cmds.f -g2012  -f sim_build/cmds.f -g2012   ../src/piso.v
-#  MODULE=test_piso TESTCASE= TOPLEVEL=piso TOPLEVEL_LANG=verilog \
-#          /sbin/vvp -M /home/fuckaduck/.local/lib/python3.9/site-packages/cocotb/libs -m libcocotbvpi_icarus   sim_build/sim.vvp
 def runtest(test):
     try:
         os.mkdir(output_dir, mode=0o755)
     except FileExistsError:
         print("[*] Output directory exists")
 
+    print("[*] Preparing iverilog_dump.v")
+    dump = open(output_dir + "iverilog_dump.v", "w")
+
+    dump.write(
+        "`timescale 1ns / 100ps\n\n"
+        + "module iverilog_dump();\n"
+        + "initial begin\n"
+        + '\t$dumpfile("'
+        + output_dir
+        + test
+        + '.vcd");\n'
+        + "\t$dumpvars(0, "
+        + test
+        + ");\n"
+        + "end\n"
+        + "endmodule\n"
+    )
+
+    dump.close()
+
     print("[*] Compiling python tests to vpp file")
 
-    rv = subprocess.run([
-        "iverilog",
-        "-o",
-        output_dir + test + ".vvp",
-        "-g2005",
-        "-D",
-        "COCOTB_SIM=1",
-        "-s",
-        test,
-        src_dir + test + ".v",
-    ])
+    rv = subprocess.run(
+        [
+            "iverilog",
+            "-o",
+            output_dir + test + ".vvp",
+            "-g",
+            "2005",
+            "-D",
+            "COCOTB_SIM=1",
+            "-s",
+            test,
+            "-s",
+            "iverilog_dump",
+            src_dir + test + ".v",
+            output_dir + "iverilog_dump.v",
+        ],
+    )
 
-    print("[*] Subprocess returned " + str(rv.returncode))
+    print(rv)
+
+    if rv.returncode != 0:
+        exit()
 
     rv = subprocess.run(
         [
@@ -57,18 +82,23 @@ def runtest(test):
         env={
             "MODULE": "test_" + test,
             "TOPLEVEL": test,
-            "TOPLEVEL_LANG": "verilog"
+            "TOPLEVEL_LANG": "verilog",
+            "COCOTB_HDL_TIMEUNIT": "1ns",
+            "COCOTB_HDL_TIMEPRECISION": "1ns",
         },
     )
 
-    print("[*] Subprocess returned " + str(rv.returncode))
+    print(rv)
+
+    if rv.returncode != 0:
+        exit()
 
 
 def runalltests():
-    for i in os.listdir(cwd):
+    for i in os.listdir("."):
         if re.search("\.py$", i) is not None and i != "run_tests.py":
             print("[*] Running " + i)
-            runtest(i[len("test_"):].split(".")[0])
+            runtest(i[len("test_") :].split(".")[0])
 
 
 delold()
