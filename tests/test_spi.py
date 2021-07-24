9c62434f46ffc1a89535d0e4cb4df24db1ee86d5
0xDEADC0DEx <48600374+0xDEADC0DEx@users.noreply.github.com>
2021-07-24 20:24:51 +0200

    Added sipo test which shows a bug in sipo

--- tests/test_spi.py
+++ tests/test_spi.py
@@ -28,41 +28,51 @@ async def start(dut):
     print("Reset done")
 
 
+# 156374987060
+# 00100100 01101000 10101100 11110001 00110100
+
+# 78187493530
+# 00010010 00110100 01010110 01111000 10011010
+
+
+async def sipo_data(dut, data):
+    for i in range(40):
+        dut.serial_in = (data & (0x1 << i)) >> i
+        await ClockCycles(dut.clk_in, clk_divider, rising=True)
+
+
 @cocotb.test()
-async def standard_mosi(dut):
+async def standard_ms_io(dut):
     await start(dut)
 
     test_data = [0x123456789A, 0xBCDEF12345, 0x6789ABCDEF, 0xDEADBEEF69]
 
     await RisingEdge(dut.r_ready_out)
     for i in range(4):
+        # Setup parallel data out
         dut.data_in = test_data[i]
+        cocotb.fork(sipo_data(dut, test_data[i]))
 
         assert int(dut.cs_out_n) == 1
 
         dut.send_enable_in = 1
-        await ClockCycles(dut.clk_in, 2 * clk_divider, rising=True)
+        await ClockCycles(dut.clk_in, clk_divider, rising=True)
 
-        assert int(dut.r_ready_out) == 0
-        assert int(dut.cs_out_n.value) == 0
-
-        print("Data start")
-        # piso test
+        # test io bit by bit
         for k in range(40):
-            print(
-                str(k)
-                + ":"
-                + str(dut.data_in.value[k])
-                + " serial:"
-                + str(int(dut.serial_out))
-            )
+            await ClockCycles(dut.clk_in, clk_divider, rising=True)
+
+            assert int(dut.r_ready_out) == 0
+            assert int(dut.cs_out_n.value) == 0
 
             assert int(dut.clk_out) == 1
             assert int(dut.data_in.value[k]) == int(dut.serial_out)
 
-            await ClockCycles(dut.clk_in, clk_divider, rising=True)
-
+        # wait for the spi to be ready
         await RisingEdge(dut.r_ready_out)
         dut.send_enable_in = 0
+
+        #  await ClockCycles(dut.clk_in, clk_divider, rising=True)
+        assert int(dut.data_out.value) == int(test_data[i])
 
         await ClockCycles(dut.clk_in, clk_divider, rising=True)
