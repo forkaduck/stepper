92329a71eec60cadf2ec06cd826e596855113ce3
0xDEADC0DEx <48600374+0xDEADC0DEx@users.noreply.github.com>
2021-07-25 22:46:10 +0200

    Removed async reset from sipo module

--- tests/test_sipo.py
+++ tests/test_sipo.py
@@ -6,21 +6,11 @@ from cocotb.triggers import RisingEdge
 @cocotb.test()
 async def test_sipo(dut):
     dut.data_in = 0
-    dut.reset_n_in = 0
     dut.r_data_out = 0
 
     # 25 MHz clock
     clock = Clock(dut.clk_in, 40, units="ns")
     cocotb.fork(clock.start())
-
-    print("Triggering reset")
-    dut.reset_n_in = 0
-    for i in range(4):
-        await RisingEdge(dut.clk_in)
-
-    dut.reset_n_in = 1
-    await RisingEdge(dut.clk_in)
-    print("Reset done")
 
     test_data = 0xABCD
 
