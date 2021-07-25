7ad75e8014dede68594ec2c7f8fe0e154a015784
0xDEADC0DEx <48600374+0xDEADC0DEx@users.noreply.github.com>
2021-07-25 22:45:16 +0200

    Rewrote piso to use a load signal instead of a reset with a counter

--- tests/test_piso.py
+++ tests/test_piso.py
@@ -5,25 +5,20 @@ from cocotb.triggers import RisingEdge
 
 @cocotb.test()
 async def test_piso(dut):
-    dut.data_in = int("10101100", 2)
+    dut.data_in = 0xAC
     dut.clk_in = 0
-    dut.reset_n_in = 1
-    dut.r_data_out = 0
+    dut.load_in = 0
 
     # 25 MHz clock
     clock = Clock(dut.clk_in, 40, units="ns")
     cocotb.fork(clock.start())
 
-    print("Triggering reset")
-    dut.reset_n_in = 0
-    for i in range(4):
-        await RisingEdge(dut.clk_in)
-
-    dut.reset_n_in = 1
     await RisingEdge(dut.clk_in)
-    print("Reset done")
+    dut.load_in = 1
+    await RisingEdge(dut.clk_in)
+    dut.load_in = 0
 
     # test simple 8 bit data conversion
     for i in range(8):
         await RisingEdge(dut.clk_in)
-        assert int(dut.data_in.value[i]) == int(dut.r_data_out)
+        assert (int(dut.data_in.value) & (0x1 << i)) >> i == int(dut.data_out)
