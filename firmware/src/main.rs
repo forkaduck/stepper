8725ad10f2a7bebb4150fa0609167ef265d61e34
0xDEADC0DEx <48600374+0xDEADC0DEx@users.noreply.github.com>
2021-08-30 12:38:20 +0200

    Added cargo build folder

---
+++ firmware/src/main.rs
@@ -0,0 +1,25 @@
+#![feature(asm)]
+#![no_std]
+#![no_main]
+
+extern crate panic_halt;
+
+use riscv_rt::entry;
+
+fn delay(cycles: u32) {
+    for _ in 0..cycles {
+        unsafe {
+            asm!("nop");
+        }
+    }
+}
+
+#[entry]
+fn main() -> ! {
+    loop {
+        unsafe {
+            *(0x10000000 as *mut u32) = !*(0x10000000 as *mut u32);
+        }
+        delay(300000);
+    }
+}
