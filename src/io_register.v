d86cafd77aeae35f3fb60adaaac3e4cb5874d559
0xDEADC0DEx <48600374+0xDEADC0DEx@users.noreply.github.com>
2021-09-04 14:31:49 +0200

    Made removed multi register support per module

--- src/io_register.v
+++ src/io_register.v
@@ -1,25 +1,20 @@
 module io_register #(
-    parameter DATA_WIDTH = 32,
-    parameter DATA_SIZE = 1,
-    parameter PATH = ""
+    parameter DATA_WIDTH = 32
 ) (
     input clk_in,
 
     // port
     input enable,
     input write,
-    input [$clog2(DATA_SIZE) -1:0] addr_in,
     input [DATA_WIDTH - 1:0] data_in,
     output reg [DATA_WIDTH - 1:0] r_data_out,
 
-    // register output
-    output reg [DATA_WIDTH - 1:0] r_mem[0:DATA_SIZE - 1]
+    output reg [DATA_WIDTH - 1:0] r_mem
 );
-
   reg [DATA_WIDTH - 1:0] r_temp;
 
   initial begin
-    if (PATH != "") $readmemh(PATH, r_mem);
+    r_mem = 'b0;
   end
 
   // handle high impedance
@@ -28,9 +23,9 @@ module io_register #(
   always @(posedge clk_in) begin
     if (enable) begin
       if (write) begin
-        r_mem[addr_in] <= data_in;
+        r_mem <= data_in;
       end else begin
-        r_temp <= r_mem[addr_in];
+        r_temp <= r_mem;
       end
     end
   end
