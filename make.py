56088a98bc5260bd28f49e9bd0ebea47d7112d64
0xDEADC0DEx <48600374+0xDEADC0DEx@users.noreply.github.com>
2021-09-10 12:35:25 +0200

    Replaced popen with run

--- make.py
+++ make.py
@@ -12,13 +12,9 @@ def run_subcommand(args, env={}):
     for i, k in env.items():
         os.environ[i] = k
 
-    rv = subprocess.Popen(args=args)
+    rv = subprocess.run(args=args, check=True)
 
     print(rv.args)
-
-    rv = rv.wait()
-    if rv:
-        sys.exit(rv)
 
 
 # Removes generated files after build
