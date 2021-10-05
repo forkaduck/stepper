d299315d5b3ff75a06f1fc43abf01d09535921e2
0xDEADC0DEx <48600374+0xDEADC0DEx@users.noreply.github.com>
2021-10-05 19:36:42 +0200

    Added gcc build command to build.rs & Added linkerscript from picorv32-rt & Added entry point to main.rs

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -3,21 +3,39 @@
 #![no_main]
 
 mod halt;
-use riscv_rt::entry;
 
-// fn delay(cycles: u32) {
-//     for _ in 0..cycles {
-//         unsafe {
-//             asm!("nop");
-//         }
-//     }
-// }
+/// Rust entry point (_start_rust)
+///
+/// Zeros bss section, initializes data section and calls main. This function
+/// never returns.
+#[link_section = ".init.rust"]
+#[export_name = "_start_rust"]
+pub unsafe extern "C" fn start_rust() -> ! {
+    // r0::zero_bss(&mut _sbss, &mut _ebss);
+    // r0::init_data(&mut _sdata, &mut _edata, &_sidata);
 
-#[entry]
+    main();
+}
+
+fn set_leds(data: u32) {
+    unsafe {
+        *(0x10000000 as *mut u32) = data;
+    }
+}
+
+fn wait(instr: u32) {
+    for _ in 0..instr {
+        unsafe {
+            asm!("nop");
+        }
+    }
+}
+
 fn main() -> ! {
     loop {
-        unsafe {
-            *(0x10000000 as *mut u32) = 0xffffffff;
-        }
+        set_leds(0xffffffff);
+        wait(25000000 / 4);
+        set_leds(0x00000000);
+        wait(25000000 / 4);
     }
 }
