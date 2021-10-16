4a0c52945c9cc64c98c90802652afd1d307a9c4a
0xDEADC0DEx <48600374+0xDEADC0DEx@users.noreply.github.com>
2021-10-16 22:42:34 +0200

    Added a toggle flip-flop to the output (50/50 clk)

--- src/clk_divider.v
+++ src/clk_divider.v
@@ -4,19 +4,26 @@ module clk_divider #(
 ) (
     input clk_in,
     input [SIZE - 1:0] max_in,
-    output reg r_clk_out
+    output clk_out
 );
 
+  reg r_internal_clk;
 
   reg [SIZE - 1:0] r_count = 0;
 
   always @(posedge clk_in) begin
-    r_clk_out <= (r_count == (max_in - 1));
+    r_internal_clk <= (r_count == ((max_in >> 1) - 1));
 
-    if (r_count == (max_in - 1)) begin
+    if (r_count == ((max_in >> 1) - 1)) begin
       r_count <= 0;
     end else begin
       r_count <= r_count + 1;
     end
   end
+
+  toggle_ff output_ff (
+    .clk_in(r_internal_clk),
+    .toggle_in(1'b1),
+    .r_q_out(clk_out)
+  );
 endmodule
