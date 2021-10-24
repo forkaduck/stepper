c2b97d8d693922eb91f47b358d4731df154e0eb7
0xDEADC0DEx <48600374+0xDEADC0DEx@users.noreply.github.com>
2021-10-24 14:42:56 +0200

    Added an assertion macro

--- src/macros.v
+++ src/macros.v
@@ -1,1 +1,8 @@
 `define FIT(bits, num) (bits > $bits(num)) ? {{bits - $bits(num){1'b0}}, num} : num[bits -1: 0]
+
+`define assert_end(signal, value) \
+        if (signal !== value) begin \
+            $display("ASSERTION FAILED in %m: signal != value"); \
+            $finish; \
+        end
+
