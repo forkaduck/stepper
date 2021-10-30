bbe7a79c8448622bfaa7133463ef39cde261fe51
0xDEADC0DEx <48600374+0xDEADC0DEx@users.noreply.github.com>
2021-10-30 21:03:38 +0200

    Fixed assertions in the pico core

--- src/macros.v
+++ src/macros.v
@@ -1,7 +1,11 @@
 `define FIT(bits, num) (bits > $bits(num)) ? {{bits - $bits(num){1'b0}}, num} : num[bits -1: 0]
 
-`define assert_end(signal, value) \
-        if (signal !== value) begin \
-            $display("ASSERTION FAILED in %m: signal != value"); \
+`ifdef __ICARUS__
+`define ASSERT(value) \
+        if ((value) != 'h1) begin \
+            $display("%0t:\tASSERTION FAILED in < %m > | < value > != 1'b1 | -> 0x%x", $time, value); \
             $finish; \
         end
+`else
+`define ASSERT(value) $display("Assertion here");
+`endif
