192f1fe2660192d22e0f11f79cdadff935dbdc81
0xDEADC0DEx <48600374+0xDEADC0DEx@users.noreply.github.com>
2021-11-07 14:36:59 +0100

    Changed shift order in sipo & Fixed test

--- src/sipo.v
+++ src/sipo.v
@@ -5,14 +5,16 @@ module sipo #(
     input data_in,
     input clk_in,
     input en_in,
-    output reg [SIZE - 1:0] r_data_out
+    output [SIZE - 1:0] data_out
 );
 
-  initial r_data_out = 'b0;
+  reg [SIZE - 1:0] r_data_out = {SIZE{1'b0}};
+
+  assign data_out = r_data_out;
 
   always @(posedge clk_in) begin
     if (en_in) begin
-      r_data_out <= {data_in, r_data_out[SIZE-1:1]};
+      r_data_out <= {r_data_out[SIZE-2:0], data_in};
       $display("%m>\tdata_in:%x r_data_out:%b", data_in, r_data_out);
     end
   end
--- tests/test_sipo.py
+++ tests/test_sipo.py
@@ -7,6 +7,7 @@ from cocotb.triggers import RisingEdge
 async def test_sipo(dut):
     dut.data_in.value = 0
     dut.r_data_out.value = 0
+    dut.en_in.value = 1
 
     # 25 MHz clock
     clock = Clock(dut.clk_in, 40, units="ns")
@@ -14,18 +15,17 @@ async def test_sipo(dut):
 
     test_data = 0xABCD
 
-    for i in range(8):
+    for i in range(7, -1, -1):
         dut.data_in.value = (test_data & (0x1 << i)) >> i
         await RisingEdge(dut.clk_in)
 
-    dut.data_in.value = 0
     await RisingEdge(dut.clk_in)
     assert dut.r_data_out.value == test_data & 0xFF
 
-    for i in range(8, 16):
+    for i in range(15, 8, -1):
         dut.data_in.value = (test_data & (0x1 << i)) >> i
         await RisingEdge(dut.clk_in)
 
-    dut.data_in.value = 0
+    await RisingEdge(dut.clk_in)
     await RisingEdge(dut.clk_in)
     assert dut.r_data_out.value == (test_data & 0xFF00) >> 8
