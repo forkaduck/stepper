688d59a0b5aa74f5374f944b1d58a9c2974289b5
0xDEADC0DEx <48600374+0xDEADC0DEx@users.noreply.github.com>
2021-11-26 11:39:41 +0100

    Fixed wrong alogrithm time scale & Refactoring

--- src/angle_to_step.v
+++ src/angle_to_step.v
@@ -6,10 +6,10 @@ module angle_to_step #(
     parameter GEARUP = 26.85,
 
     parameter SYSCLK = 25000000,
-    parameter DIVMAX = 32,
-    parameter SIZE   = 64,
-    parameter VRISE  = 250,
-    parameter TRISE  = 2500
+    parameter SIZE = 64,
+    parameter VRISE = 25000.0,
+    parameter TRISE = 100.0,
+    parameter VOFFSET = 10.0
 ) (
     input clk_i,
     input reset_n_i,
@@ -18,11 +18,7 @@ module angle_to_step #(
     input [31:0] relative_angle_i,
     output step_o
 );
-  parameter FFIX = 2;
-  parameter SHIFT_VRISE = VRISE << FFIX;
-  parameter SHIFT_TRISE = TRISE << FFIX;
-
-  parameter JERCK = (4 * SHIFT_VRISE / SHIFT_TRISE ^ 2);
+  parameter JERCK = (4.0 * (VRISE - VOFFSET) / $pow(TRISE, 2.0));
 
   wire int_clk;
   wire output_clk;
@@ -32,13 +28,15 @@ module angle_to_step #(
   reg [SIZE - 1:0] r_div = {SIZE{1'b0}};
   reg [SIZE - 1:0] r_t = {SIZE{1'b0}} | 1'b1;
 
+  reg [1:0] ding = 0;
+
   // Internal clk
   clk_divider #(
       .SIZE(32)
   ) internal (
       .clk_in (clk_i),
       // Every 1 us
-      .max_in (SYSCLK / 1000000),
+      .max_in ((SYSCLK / 25000.0) / 2),
       .clk_out(int_clk)
   );
 
@@ -47,27 +45,33 @@ module angle_to_step #(
       .SIZE(SIZE)
   ) div (
       .clk_in (clk_i),
-      .max_in (VRISE - (r_div >> FFIX)),
+      .max_in (VRISE - r_div),
       .clk_out(output_clk)
   );
 
   // Update the clkdivider output every int_clk
   always @(posedge int_clk, negedge reset_n_i) begin
     if (!reset_n_i) begin
-      r_t <= 1;
+      r_t <= 1.0;
     end else begin
-      if (r_t > 0 && r_t < TRISE / 2) begin
-        r_div = 2 * JERCK * $pow(r_t, 2);
+      if (r_t > 0 && r_t < (TRISE / 2)) begin
+        // Wenn(x < t_{rise} / 2 ∧ x > 0, 1 / 2 J x²)
+        r_div = 0.5 * JERCK * $pow(r_t, 2.0) + VOFFSET;
+        ding <= 1;
 
-      end else if (r_t >= TRISE / 2 && r_t <= TRISE) begin
-        r_div = 1 * JERCK * (4 * r_t * SHIFT_TRISE - 2 * $pow(r_t, 2) - $pow(SHIFT_TRISE, 2));
+      end else if (r_t >= (TRISE / 2) && r_t <= TRISE) begin
+        // Wenn(t_{rise} / 2 ≤ x ∧ x < t_{rise}, 1 / 4 J (4x t_{rise} - 2x² - t_{rise}²))
+        r_div = 0.25 * JERCK * (r_t * 4.0 * TRISE - 2.0 * $pow(r_t, 2.0) - $pow(TRISE, 2.0)) +
+            VOFFSET;
+        ding <= 2;
 
       end else if (r_t > TRISE) begin
-        r_div = VRISE;
+        r_div = (VRISE - VOFFSET);
+        ding <= 3;
 
       end
 
-      r_t <= r_t + 1;
+      r_t <= r_t + 1.0;
     end
   end
 endmodule
