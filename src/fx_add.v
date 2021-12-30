64ff4ade92ea03c9533700066255b4fa79d12cf2
0xDEADC0DEx <48600374+0xDEADC0DEx@users.noreply.github.com>
2021-12-30 16:02:44 +0100

    Further clearup && Added div and mult modules

--- src/fx_add.v
+++ src/fx_add.v
@@ -1,70 +1,72 @@
-// Originally from https://github.com/freecores/verilog_fixed_point_math_library
+// Originally from https://github.com/freecoresult/verilog_fixed_point_math_library
+// Modified to be readable
 
 module fx_add #(
     parameter Q = 15,
     parameter N = 32
 ) (
-    input  [N-1:0] a,
-    input  [N-1:0] b,
-    output [N-1:0] c
+    // Numbers
+    input  [N-1:0] summand_a_i,
+    input  [N-1:0] summand_b_i,
+    output [N-1:0] sum_i
 );
 
-  reg [N-1:0] res;
+  reg [N-1:0] result;
 
-  assign c = res;
+  assign sum_i = result;
 
-  always @(a, b) begin
+  always @(summand_a_i, summand_b_i) begin
     // Both negative or both positive
     // Since they have the same sign, absolute magnitude increases
-    if (a[N-1] == b[N-1]) begin
+    if (summand_a_i[N-1] == summand_b_i[N-1]) begin
       // So we just add the two numbers and set the sign appropriately.
       // Doesn't matter which one we use, they both have the same sign.
-      res[N-2:0] = a[N-2:0] + b[N-2:0];
+      result[N-2:0] = summand_a_i[N-2:0] + summand_b_i[N-2:0];
 
       // Do the sign last, on the off-chance there was an overflow.
-      res[N-1]   = a[N-1];
+      result[N-1]   = summand_a_i[N-1];
     end else begin
-      // One of them is negative...
-      if (a[N-1] == 0 && b[N-1] == 1) begin
+      // One of them is negative
+      if (summand_a_i[N-1] == 0 && summand_b_i[N-1] == 1) begin
         // Subtract a-b
-        if (a[N-2:0] > b[N-2:0]) begin
-          // If a is greater than b, then just subtract b from a
+        if (summand_a_i[N-2:0] > summand_b_i[N-2:0]) begin
+          // If summand_a_i is greater than b, then just subtract b from a
           // and manually set the sign to positive.
-          res[N-2:0] = a[N-2:0] - b[N-2:0];
-          res[N-1]   = 0;
+          result[N-2:0] = summand_a_i[N-2:0] - summand_b_i[N-2:0];
+          result[N-1]   = 0;
 
         end else begin
           // If a is less than b,
           // we'll actually subtract a from b to avoid a 2's complement
           // answer.
-          res[N-2:0] = b[N-2:0] - a[N-2:0];
+          result[N-2:0] = summand_b_i[N-2:0] - summand_a_i[N-2:0];
 
           // Avoid negative 0 by unsetting the last bit.
-          if (res[N-2:0] == 0) begin
-            res[N-1] = 0;
+          if (result[N-2:0] == 0) begin
+            result[N-1] = 0;
           end else begin
-            res[N-1] = 1;
+            result[N-1] = 1;
           end
 
         end
       end else begin
         // Subtract b-a (a negative, b positive)
-        if (a[N-2:0] > b[N-2:0]) begin
+        if (summand_a_i[N-2:0] > summand_b_i[N-2:0]) begin
           // If a is greater than b,
           // we'll actually subtract b from a to avoid a 2's complement answer
           // and manually set the sign to negative
-          res[N-2:0] = a[N-2:0] - b[N-2:0];
+          result[N-2:0] = summand_a_i[N-2:0] - summand_b_i[N-2:0];
 
-          if (res[N-2:0] == 0) begin
-            res[N-1] = 0;
+          if (result[N-2:0] == 0) begin
+            result[N-1] = 0;
           end else begin
-            res[N-1] = 1;
+            result[N-1] = 1;
           end
         end else begin
           //If a is less than b, then just subtract a from b
           //and manually set the sign to positive
-          res[N-2:0] = b[N-2:0] - a[N-2:0];
-          res[N-1]   = 0;
+          result[N-2:0] = summand_b_i[N-2:0] - summand_a_i[N-2:0];
+          result[N-1]   = 0;
         end
       end
     end
