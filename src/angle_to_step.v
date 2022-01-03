1dc2eb4de3facb0b4380eb8686be1aee013c5e29
0xDEADC0DEx <48600374+0xDEADC0DEx@users.noreply.github.com>
2022-01-03 09:55:25 +0100

    Added some comments && Removed useless reset line

--- src/angle_to_step.v
+++ src/angle_to_step.v
@@ -6,13 +6,12 @@ module angle_to_step #(
     parameter GEARUP = 26.85,
 
     parameter SYSCLK = 25000000,
-    parameter SIZE = 128,
+    parameter SIZE = 32,
     parameter [SIZE - 1:0] VRISE = 20000,
     parameter [SIZE - 1:0] TRISE = 10000,
     parameter [SIZE - 1:0] VOFFSET = 6000
 ) (
     input clk_i,
-    input reset_n_i,
 
     input enable_i,
     input [SIZE - 1:0] relative_angle_i,
@@ -32,12 +31,11 @@ module angle_to_step #(
 
   assign step_o = enable_i ? output_clk : 1'b0;
 
-  // Split the result into multiple registers (easier debugging)
+  // Used as the actual frequency divider (inverse)
   reg [SIZE - 1:0] r_div = {SIZE{1'b0}};
-  reg [SIZE - 1:0] r_div1 = {SIZE{1'b0}};
-  reg [SIZE - 1:0] r_div2 = {SIZE{1'b0}};
-  reg [SIZE - 1:0] r_div3 = {SIZE{1'b0}};
-  reg [SIZE - 1:0] r_debug = {SIZE{1'b0}};
+
+  reg [SIZE - 1:0] r_stage1 = {SIZE{1'b0}};
+  reg [SIZE - 1:0] r_stage2 = {SIZE{1'b0}};
 
   reg [SIZE - 1:0] r_t = TIME_INC;
 
@@ -49,7 +47,7 @@ module angle_to_step #(
   // Indicates the current state of the machine (debugging)
   reg [1:0] state = 1'b0;
 
-  // Internal clk
+  // Internal clk (used for timekeeping)
   clk_divider #(
       .SIZE(32)
   ) internal (
@@ -59,7 +57,7 @@ module angle_to_step #(
       .clk_out(int_clk)
   );
 
-  // Output clk divider
+  // Step pulse generator clk divider
   clk_divider #(
       .SIZE(SIZE)
   ) div (
@@ -72,19 +70,14 @@ module angle_to_step #(
   always @(posedge clk_i) begin
     if (r_t > 0 && r_t < (SF_TRISE >> 1)) begin
       //First rise
-      // Wenn(x < t_{rise} / 2 ∧ x > 0, 1 / 2 J x²)
-      /* r_div <= ('b0_1000 << SF) * JERCK * r_t ** 2; */
-      r_div  <= r_div1 * r_div2 * r_div3;
-      r_div1 <= ('b0_1000 << SF);
-      r_div2 <= JERCK;
-      r_div3 <= r_t * r_t;
-
-      state  <= 1;
+      // If(x < t_{rise} / 2 ∧ x > 0, 1 / 2 J x²)
+      r_div <= r_stage1;
+      state <= 1;
 
     end else if (r_t >= (SF_TRISE >> 1) && r_t < SF_TRISE) begin
       // Second rise
-      // Wenn(t_{rise} / 2 ≤ x ∧ x < t_{rise}, 1 / 4 J (4x t_{rise} - 2x² - t_{rise}²))
-      r_div <= ('b0_0100 << SF) * JERCK * ((4 << SF) * r_t * SF_TRISE - (2 << SF) * (r_t ** 2) - SF_TRISE ** 2);
+      // If(t_{rise} / 2 ≤ x ∧ x < t_{rise}, 1 / 4 J (4x t_{rise} - 2x² - t_{rise}²))
+      r_div <= r_stage2;
       state <= 2;
 
     end else begin
@@ -92,42 +85,33 @@ module angle_to_step #(
       r_div = SF_VRISE;
       state <= 3;
     end
-    r_debug <= SF;
   end
 
-  always @(posedge output_clk, negedge reset_n_i) begin
-    if (!reset_n_i) begin
+  // Counter to keep track of how far the algorithm has already stepped.
+  // It is used to find out when the algorithm needs to be reversed for the falloff.
+  always @(posedge output_clk) begin
+    if (steps_needed == steps_done) begin
       steps_done <= 0;
     end else begin
-
-      if (steps_needed == steps_done) begin
-        steps_done <= 0;
-      end else begin
-        steps_done <= steps_done + 1;
-      end
+      steps_done <= steps_done + 1;
     end
   end
 
-  always @(posedge int_clk, negedge reset_n_i) begin
-    if (!reset_n_i) begin
-      r_t <= TIME_INC;
-      steps_needed <= 0;
-    end else begin
-      if (r_t >= SF_TRISE && (steps_needed / 2) <= steps_done) begin
-        count_back <= 1'b1;
-      end else if (r_t == 0) begin
-        count_back <= 1'b0;
-      end
+  always @(posedge int_clk) begin
+    if (r_t >= SF_TRISE && (steps_needed / 2) <= steps_done) begin
+      count_back <= 1'b1;
+    end else if (r_t == 0) begin
+      count_back <= 1'b0;
+    end
 
-      if (enable_i) begin
-        if (count_back) begin
-          r_t <= r_t - TIME_INC;
-        end else begin
-          r_t <= r_t + TIME_INC;
-        end
+    if (enable_i) begin
+      if (count_back) begin
+        r_t <= r_t - TIME_INC;
       end else begin
-        r_t <= TIME_INC;
+        r_t <= r_t + TIME_INC;
       end
+    end else begin
+      r_t <= TIME_INC;
     end
 
     steps_needed <= (relative_angle_i * GEARUP) / STEPANGLE * MICROSTEPS;
