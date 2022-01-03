84ca50c19d789c538327ade81872812f73438b3b
0xDEADC0DEx <48600374+0xDEADC0DEx@users.noreply.github.com>
2022-01-03 10:19:29 +0100

    Removed reset from test

--- tests/test_angle_to_step.py
+++ tests/test_angle_to_step.py
@@ -5,22 +5,11 @@ from cocotb.triggers import RisingEdge, ClockCycles
 
 @cocotb.test()
 async def test_angle_to_step(dut):
-    dut.reset_n_i.value = 1
     dut.enable_i.value = 1
     dut.relative_angle_i.value = 10
 
     # 25 MHz clock
     clock = Clock(dut.clk_i, 40, units="ns")
     cocotb.fork(clock.start())
-
-    print("Triggering reset")
-    dut.reset_n_i.value = 1
-    await ClockCycles(dut.clk_i, 10, rising=True)
-
-    dut.reset_n_i.value = 0
-    await ClockCycles(dut.clk_i, 100, rising=True)
-
-    dut.reset_n_i.value = 1
-    print("Reset done")
 
     await ClockCycles(dut.clk_i, 200000, rising=True)
