e599c26ff709656b088fbe1cded2f81a12440580
0xDEADC0DEx <48600374+0xDEADC0DEx@users.noreply.github.com>
2022-03-17 17:41:47 +0100

    Added inv_kin test && Added internal signals to stepper.gtkw

--- tests/test_inv_kin.py
+++ tests/test_inv_kin.py
@@ -5,7 +5,10 @@ from cocotb.triggers import RisingEdge, ClockCycles
 
 @cocotb.test()
 async def test_inv_kin(dut):
-    dut.x_in = 10
+    dut.x_in.value = 0x0000000a00000000
+    dut.y_in.value = 0x0000000a00000000
+    dut.z_in.value = 0x0000000a00000000
+
     # 25 MHz clock
     clock = Clock(dut.clk_in, 40, units="ns")
     cocotb.fork(clock.start())
