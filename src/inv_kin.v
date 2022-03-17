41f8f6b2c9b32719f3cf31b9040376a2c4d73ee2
0xDEADC0DEx <48600374+0xDEADC0DEx@users.noreply.github.com>
2022-03-17 17:36:23 +0100

    Implemented the first set of equations

--- src/inv_kin.v
+++ src/inv_kin.v
@@ -1,17 +1,52 @@
 
 module inv_kin #(
-    parameter Q = 15,
-    parameter N = 32
+    parameter Q = 32,
+    parameter N = 64,
+
+    parameter UPPER_HIP_LEN   = 10,
+    parameter UPPER_THIGH_LEN = 10,
+    parameter LOWER_THIGH_LEN = 10
 ) (
     input clk_in,
     input [N -1:0] x_in,
     input [N -1:0] y_in,
     input [N -1:0] z_in,
 
-    output [N -1:0] one_out,
-    output [N -1:0] sec_out,
-    output [N -1:0] third_out
+    output [N -1:0] alpha_out,
+    output [N -1:0] beta_out,
+    output [N -1:0] gamma_out
 );
+
+  // Calculate the angle alpha (the hip joint)
+  wire [N-1:0] alpha_pyth;
+  pythagoras #(
+      .Q(Q),
+      .N(N)
+  ) calc_alpha_pyth (
+      .clk_in(clk_in),
+      .x_in  (x_in),
+      .y_in  (y_in),
+
+      .len_out(alpha_pyth),
+      .overflow_out(),
+      .valid_out()
+  );
+
+  wire [N-1:0] alpha_div;
+  fx_div #(
+      .Q(Q),
+      .N(N)
+  ) calc_alpha_div (
+      .dividend_in (x_in),
+      .divisor_in  (alpha_pyth),
+      .quotient_out(alpha_div),
+
+      .start_in(1'b1),
+      .clk_in  (clk_in),
+
+      .complete_out(),
+      .overflow_out()
+  );
 
   read_func #(
 `ifdef __ICARUS__
@@ -20,10 +55,10 @@ module inv_kin #(
       .PATH("math_functions/arccos.mem"),
 `endif
       .DATA_SIZE(200),
-      .DATA_WIDTH(32)
+      .DATA_WIDTH(N)
   ) arccos (
-      .x_in (x_in),
-      .y_out(one_out)
+      .x_in (alpha_div >> Q),  // shift??
+      .y_out(alpha_out)
   );
 
 endmodule
--- src/pythagoras.v
+++ src/pythagoras.v
@@ -1,22 +1,19 @@
-
 module pythagoras #(
-    parameter Q = 15,
-    parameter N = 32
+    parameter Q = 32,
+    parameter N = 64
 ) (
+    input clk_in,
     input [N-1:0] x_in,
     input [N-1:0] y_in,
 
     output [N-1:0] len_out,
-    output overflow
+    output overflow_out,
+    output valid_out
 );
+
 
   wire [N-1:0] x_power_two;
   wire x_overflow;
-
-  wire [N-1:0] y_power_two;
-  wire y_overflow;
-
-  assign overflow = y_overflow || x_overflow;
 
   // x_power_two = x^2
   fx_mult #(
@@ -29,6 +26,9 @@ module pythagoras #(
       .overflow_r_out(x_overflow)
   );
 
+  wire [N-1:0] y_power_two;
+  wire y_overflow;
+
   // y_power_two = y^2
   fx_mult #(
       .Q(Q),
@@ -40,6 +40,8 @@ module pythagoras #(
       .overflow_r_out(y_overflow)
   );
 
+  wire [N-1:0] x_y_add;
+
   // len_out = x_power_two + y_power_two
   fx_add #(
       .Q(Q),
@@ -47,7 +49,21 @@ module pythagoras #(
   ) calc_x_y_add (
       .summand_a_in(x_power_two),
       .summand_b_in(y_power_two),
-      .sum_out(len_out)
+      .sum_out(x_y_add)
   );
 
+  assign overflow_out = y_overflow || x_overflow;
+
+  read_func #(
+`ifdef __ICARUS__
+      .PATH("../math_functions/sqrt.mem"),
+`else
+      .PATH("math_functions/sqrt.mem"),
+`endif
+      .DATA_SIZE(200),
+      .DATA_WIDTH(N)
+  ) calc_x_y_sqrt (
+      .x_in (x_y_add >> Q),
+      .y_out(len_out)
+  );
 endmodule
